847bf34da930e16eaaeaf96e2a105e65bf751976
seohyun0620 <tjgus7924@naver.com>
2024-03-04 23:25:42 +0900

    Promise와 제네릭의 관계 파악 및 enum타입 부여

--- learn-typescript/quiz/2_address-book/src/index.ts
+++ learn-typescript/quiz/2_address-book/src/index.ts
@@ -1,3 +1,11 @@
+//enum타입을 사용하여 파라미터에 잘못된 값이 들어가더라도 에러가 발생하지 x
+//전화번호부 유형 (home,office,studio)을 enum타입으로 선언
+enum PhoneType {
+  Home = 'home',
+  Office = 'office',
+  Studio = 'studio',
+}
+
 interface PhoneNumberDictionary {
   [phone: string]: {
     num: number;
@@ -12,6 +20,8 @@ interface Contact {
 
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
+//Promise는 인터페이스로 정의되어 있고 제네릭 타입을 받을 수 있음.(Promise에 커서 올리고 ctrl+마우스왼쪽 누르면 Promise객체의 타입 정의 보기 가능)
+//제네릭으로 정의된 T를 .then()안에서 T로 받음
 function fetchContacts(): Promise<Contact[]> {
   // TODO: 아래 변수의 타입을 지정해보세요.
   const contacts: Contact[] = [
@@ -70,15 +80,16 @@ class AddressBook {
   }
 
   /* TODO: 아래 함수들의 파라미터 타입과 반환 타입을 지정해보세요 */
+
   findContactByName(name: string): Contact[] {
-    return this.contacts.filter(contact => contact.name === name);
+    return this.contacts.filter(contact => contact.name === name); //this.contacts는 클래스 속성 contacts를 가르킴
   }
 
   findContactByAddress(address: string): Contact[] {
     return this.contacts.filter(contact => contact.address === address);
   }
 
-  findContactByPhone(phoneNumber: number, phoneType: string): Contact[] {
+  findContactByPhone(phoneNumber: number, phoneType: PhoneType): Contact[] {
     return this.contacts.filter(
       contact => contact.phones[phoneType].num === phoneNumber
     );
@@ -98,4 +109,7 @@ class AddressBook {
   /* ------------------------------------------------ */
 }
 
-new AddressBook();
+const book = new AddressBook();
+
+//두 번째 파라미터 타입에 enum타입을 넣어주어야 함
+book.findContactByPhone(11122223333, PhoneType.Home);
